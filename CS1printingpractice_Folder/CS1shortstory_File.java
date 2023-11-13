3d1aa4652e0d5a5a041314efc382d01fbdde31f2
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2023-11-13 15:00:56 +0000

    jdlorfhorhbjtr

---
+++ CS1printingpractice_Folder/CS1shortstory_File.java
@@ -0,0 +1,15 @@
+class CS1shortstory_File
+{
+    public static void main (String args[])
+    {
+        System.out.println("'The boring man'");
+        System.out.println("      ");
+        System.out.println("There once was a man.");
+        System.out.println("      ");
+        System.out.println("He was very boring.");
+        System.out.println("      ");
+        System.out.println("Then he died.");
+        System.out.println("      ");
+        System.out.println("Thank you for reading 'The boring man'");
+    }
+}
