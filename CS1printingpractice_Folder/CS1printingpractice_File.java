0060907b0c899e4d9f1a3ac3b3537f10e5d6a0eb
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2023-11-13 14:30:59 +0000

    bebedeepadoo

---
+++ CS1printingpractice_Folder/CS1printingpractice_File.java
@@ -0,0 +1,13 @@
+class CS1printingpractice_File
+{
+    public static void main (String args[])
+    {
+        System.out.println("Welcome");
+        System.out.println("    ");
+        System.out.println("Name: Brayden");
+        System.out.println("    ");
+        System.out.println("Age: 15");
+        System.out.println("    ");
+        System.out.println("Unwelcome");
+    }
+}
