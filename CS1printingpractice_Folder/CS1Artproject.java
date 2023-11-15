41fc6a5077433d493cfb915466ec2313c9687254
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2023-11-15 14:12:43 +0000

    hooray

---
+++ CS1printingpractice_Folder/CS1Artproject.java
@@ -0,0 +1,13 @@
+class CS1Artproject
+{
+    public static void main(String args[])
+    {
+        System.out.println(" *** *** ");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println(" ******* ");
+        System.out.println("  *****  ");
+        System.out.println("   ***   ");
+        System.out.println("    *    ");
+    }
+}
