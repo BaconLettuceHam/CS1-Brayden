72f9e925caa354a042b87a22912f835b12d53e8a
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2023-11-15 15:19:31 +0000

    dodododododod

---
+++ CS1miniproblems_Folder/CS1mini11.java
@@ -0,0 +1,11 @@
+class CS1mini11
+{
+    public static void main(String args[])
+    {
+        System.out.println("    *");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*    ");
+    }
+}
