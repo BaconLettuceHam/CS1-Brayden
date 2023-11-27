14e5696c008b9f85e35f2cb211bb210622a40333
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2023-11-27 15:15:36 +0000

    aw yeah

---
+++ CS1madlibs_Folder/madlibs.java
@@ -0,0 +1,24 @@
+class madlibs
+{
+    public static void main (String args[])
+    {
+        String one = "one";
+        String two = "fish";
+        String three = "sneaking sus";
+        String four = "folly";
+        String five = "adjective";
+        String six = "serendipious";
+        String seven = "windows";
+        String eight = "yucky";
+        String nine = "airplane";
+        String ten = "absolutely not";
+
+        System.out.println("Once there was a "+ one +" and a "+ two);
+        System.out.println("One day the "+ one +" found a "+ three +", so they "+ four);
+        System.out.println("After "+ two +" came back from "+ five +", they saw that "+ one +" had found a "+ three);
+        System.out.println("Just then, a "+ six +" came and "+ seven +" the "+ three );
+        System.out.println("After that, "+ one +" deided to "+ eight +" while "+ two + " decided to " + nine);
+        System.out.println("The "+ ten +" was never the same again.");
+
+    }
+}
