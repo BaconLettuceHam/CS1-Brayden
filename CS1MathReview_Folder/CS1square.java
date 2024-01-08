5a71957903fda26cddf291c57af38c1ddf8a7ebf
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2024-01-08 16:28:31 +0000

    jireonjd

---
+++ CS1MathReview_Folder/CS1square.java
@@ -0,0 +1,16 @@
+//Brayden Hawkins
+//1/8/2024
+//Computer Science 1
+//squares a number, then halves it
+
+class CS1square
+{
+    public static void main (String args[])
+    {
+        //assigning variables
+        int num1=6;
+        int sqr=num1*num1;
+        float hlv=sqr/2;
+        System.out.print(hlv);
+    }
+}
