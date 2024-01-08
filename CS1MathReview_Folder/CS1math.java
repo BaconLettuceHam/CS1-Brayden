c9ceabcbad726fea569bc7d8b7ec44182a7a88b8
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2024-01-08 16:20:46 +0000

    ujdkbffejirw

---
+++ CS1MathReview_Folder/CS1math.java
@@ -0,0 +1,21 @@
+//Brayden Hawkins
+//1/8/2024
+//Computer Science 1
+//Averaged four numbers together
+
+class CS1math
+{
+    public static void main (String args[])
+    {
+        //assigning datatypes
+        int num1, num2, num3, num4;
+        int ans;
+        //assigning variables
+        num1=4;
+        num2=8;
+        num3=16;
+        num4=32;
+        ans=num1+num2+num3+num4;
+        System.out.print(ans);
+    }
+}
