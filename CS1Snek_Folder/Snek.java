96d64056d53b0c40ab8a13cdc301fbbbb30cb09a
BaconLettuceHam <149608225+BaconLettuceHam@users.noreply.github.com>
2024-05-19 01:33:24 +0000

    ghvh

--- CS1Snek_Folder/Snek.java
+++ CS1Snek_Folder/Snek.java
@@ -1,6 +1,6 @@
 //SOURCE:https://www.youtube.com/watch?v=bI6e6qjJ8JQ
 public class Snek{
-    public static void main(String args[]){
+    public static void main(String[] args){
         new GameFrame();
     }
 
